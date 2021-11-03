a1d4cc07b460368a5271cf79475cb6bd1df825e0
Ian <motolegacy.git@gmail.com>
2021-11-03 16:02:58 -0400

    Remove bunk request handler and convert analog input to bytes

--- teensy-controller.ino
+++ teensy-controller.ino
@@ -7,6 +7,7 @@
 // You should have received a copy of the license along with this
 // work. If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
 //
+#include <HardwareSerial.h>
 
 //
 // Globals
@@ -17,15 +18,33 @@ int PSP_SERIAL_PIN_Y            = 1;            // The pin responsible for the s
 int PSP_SERIAL_REQ_BOTH         = 2;            // The request byte for when a user wants to receive both axes
 int PSP_SERIAL_REQ_IS_ACTIVE    = 3;            // Request byte for checking if serial is active
 
+// TODO: Investigate why this is so.. weird
+int ANALOG_X_MAXIMUM            = 750;
+int ANALOG_X_MINIMUM            = 128;
+int ANALOG_Y_MAXIMUM            = 861;
+int ANALOG_Y_MINIMUM            = 265;
+
+void boot_text()
+{
+  Serial.println("=================================================");
+  Serial.println(" ___________      ______ _____ _____ _____ _____ ");
+  Serial.println("|  _  | ___ \\     |  _  \\_   _|_   _|_   _|  _  |");
+  Serial.println("| | | | |_/ /_____| | | | | |   | |   | | | | | |");
+  Serial.println("| | | |  __/______| | | | | |   | |   | | | | | |");
+  Serial.println("\\ \\_/ / |         | |/ / _| |_  | |   | | \\ \\_/ /");
+  Serial.println(" \\___/\\_|         |___/  \\___/  \\_/   \\_/  \\___/ ");
+  Serial.println("=================================================");
+}
+
 //
 // Setup
-// Called on initialization, set pins up and
-// turn the LED on.
+// Called on initialization, set up pins and turn
+// the LED on.
 //
 void setup()
 {
-    // Print that we're booted up
-    Serial.println("Teensy Started!");
+    // Epic Boot Text
+    boot_text();
 
     // Set up our pin modes for the X and Y input axes
     pinMode(PSP_SERIAL_PIN_X, INPUT_PULLUP);
@@ -35,35 +54,37 @@ void setup()
     Serial1.begin(PSP_SERIAL_BAUD);
 
     // Turn pin6 (LED) ON
-    digialWrite(6, HIGH);
+    digitalWrite(6, HIGH);
 }
 
 //
 // Loop
-// Executed ever 2ms, handle requests sent to
-// the Teensy.
+// Executed every 5mis, handle UART requests
+// sent to the Teensy and do analog conversions.
 //
 void loop()
 {
-    // Check for Requests
-    int request = Serial1.read();
+    // Grab the X Axis
+    int analog_x = analogRead(PSP_SERIAL_PIN_X);
+    analog_x = map(analog_x, ANALOG_X_MINIMUM, ANALOG_X_MAXIMUM, 0, 255);
 
-    // If we're requesting either axis, go ahead and shoot the analog state back
-    if (request == PSP_SERIAL_PIN_X || request == PSP_SERIAL_PIN_Y) {
-        Serial1.write(analogRead(request));
-    }
+    if (analog_x > 255) analog_x = 255;
+    if (analog_x < 0) analog_x = 0;
 
-    // If there's any sync issues, user will request both
-    if (request == PSP_SERIAL_REQ_BOTH) {
-        Serial1.write(analogRead(PSP_SERIAL_PIN_X));
-        Serial1.write(analogRead(PSP_SERIAL_PIN_Y));
-    }
+    byte analog_x_byte = byte(analog_x); // put it in byte form for later
 
-    // Check if we're Active
-    if (request == PSP_SERIAL_REQ_IS_ACTIVE) {
-        Serial1.write(byte(1));
-    }
+    // Grab the Y Axis
+    int analog_y = analogRead(PSP_SERIAL_PIN_Y);
+    analog_y = map(analog_y, ANALOG_Y_MINIMUM, ANALOG_Y_MAXIMUM, 0, 255);
+
+    if (analog_y > 255) analog_y = 255;
+    if (analog_y < 0) analog_y = 0;
+
+    byte analog_y_byte = byte(analog_y); // put it in byte form for later
+
+    Serial.println(analog_x_byte);
+    Serial.println(analog_y_byte);
 
     // Delay for 5 microseconds
-    delayMicroseconds(5);
+    delayMicroseconds(500);
 }
